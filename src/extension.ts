18d7a071fad13a581ae60e2e4204b573edd654c9
Pho Hale <PhoHale@gmail.com>
2024-02-28 18:25:58 -0500

    added a test status bar item that displays the current best parsed symbol from the current selection in VSCode

--- src/extension.ts
+++ src/extension.ts
@@ -5,6 +5,7 @@ import * as path from 'path';
 import { basename } from 'path';
 import { getRelatedDefinedSymbols } from './utils/getRelatedDefinedSymbols';
 import { getCurrentFileDottedPath } from './utils/getCurrentFileDottedPath';
+import { getLastNPeriodSeparatedElements, findSymbolInString } from './utils/dottedPathHelpers';
 import { SymbolProvider } from './SymbolProvider';
 
 class NoWorkspaceOpen extends Error {
@@ -74,120 +75,6 @@ async function getContainingSymbol(lineNumber: number, symbols: vscode.DocumentS
 	return undefined;
 }
 
-
-
-// async function copyCurrentLanguageServerSymbols() {
-// 	// Not needed. copies the current symbol using the language server
-// 	"# From `pyphocorehelpers.indexing_helpers.safe_find_index_in_list`"
-
-
-// 	if (!vscode.workspace.rootPath) {
-// 		throw new Error("NoWorkspaceOpen");
-// 	}
-
-// 	let editor = vscode.window.activeTextEditor;
-// 	if (!editor) {
-// 		throw new Error("NoTextEditorOpen");
-// 	}
-
-// 	let document = editor.document;
-// 	if (document.isUntitled) {
-// 		throw new Error("DocumentIsUntitled");
-// 	}
-
-// 	// const path = document.uri.path;
-
-// 	const lineNumber = editor.selection.active.line;
-// 	let symbols = await vscode.commands.executeCommand<vscode.DocumentSymbol[]>(
-// 		'vscode.executeDocumentSymbolProvider',
-// 		document.uri
-// 	);
-
-// 	let debug_string: string[] = [];
-// 	let symbol_strings: string[] = [];
-
-// 	if (symbols) {
-// 		debug_string.push("All symbols:");
-// 		console.log("All symbols:");
-// 		output_channel.appendLine("All symbols:");
-
-// 		// await printAllSymbols(symbols, res);
-// 		// let symbol = await getContainingSymbol(lineNumber, symbols, res);
-// 		await printAllSymbols(symbols);
-
-// 		for (let symbol of symbols) {
-// 			console.log(`Symbol name: ${symbol.name}, Range: l(${symbol.range.start.line}, ${symbol.range.start.character}) - l(${symbol.range.end.line}, ${symbol.range.end.character}), Kind: ${vscode.SymbolKind[symbol.kind]}`);
-// 			output_channel.appendLine(`Symbol name: ${symbol.name}, Range: l(${symbol.range.start.line}, ${symbol.range.start.character}) - l(${symbol.range.end.line}, ${symbol.range.end.character}), Kind: ${vscode.SymbolKind[symbol.kind]}`);
-
-// 			if (symbol.children) {
-// 				await printAllSymbols(symbol.children);
-// 			}
-// 		}
-
-// 		// const symbolProvider = new SymbolProvider(symbols);
-
-// 		// Get best symbol
-// 		let best_containing_symbol = await getContainingSymbol(lineNumber, symbols, false); // full_symbol_path = True
-// 		if (best_containing_symbol) {
-// 			let lineSymbolText = `Symbol at line ${lineNumber}: ${vscode.SymbolKind[best_containing_symbol.kind]} ${best_containing_symbol.name}`;
-// 			console.log(lineSymbolText);
-// 			output_channel.appendLine(lineSymbolText);
-// 			debug_string.push(`\n${lineSymbolText}`);
-
-// 			let lineSymbolText = `Symbol at line ${lineNumber}: ${vscode.SymbolKind[best_containing_symbol.kind]} ${best_containing_symbol.name}`;
-
-// 			// get best symbol
-// 			// get current file dotted path
-// 			// const currentFileDottedPath = getCurrentFileDottedPath({ rootPath: folder.uri.fsPath, currentFilePath: currentFilePath, shouldAddModuleRootName});
-// 			try {
-// 				const resource = editor.document.uri;
-// 				if (resource.scheme === 'file') {
-// 					const currentFilePath = editor.document.fileName;
-// 					if (!currentFilePath) {
-// 						vscode.window.showErrorMessage("Don't read file. only use this command when selected file.");
-// 						return;
-// 					}
-// 					if (!/.py$/.test(currentFilePath)) {
-// 						vscode.window.showErrorMessage('Not a python file. only use this command when selected python file.');
-// 						return;
-// 					}
-// 					// Get workspace folder to determine relative path
-// 					const folder = vscode.workspace.getWorkspaceFolder(resource);
-// 					if (!folder) {
-// 						vscode.window.showErrorMessage('No workspace folder is opened. only use this command in a workspace.');
-// 						return;
-// 					}
-// 					// get current file dotted path
-// 					const currentFileDottedPath = getCurrentFileDottedPath({ rootPath: folder.uri.fsPath, currentFilePath: currentFilePath, shouldAddModuleRootName: false });
-// 					output_channel.appendLine(`currentFileDottedPath: ${currentFileDottedPath}`);
-// 					// get related defined symbols from current file and current cursor position
-// 					const text = vscode.window.activeTextEditor!.document.getText();
-// 					const currentLine = vscode.window.activeTextEditor!.selection.active.line;
-// 					const definedSymbols = getRelatedDefinedSymbols(text, currentLine + 1);
-// 					const finalOutPath = [currentFileDottedPath, ...definedSymbols].join('.');
-// 					// copy python dotted path to clipboard
-// 					await vscode.env.clipboard.writeText(finalOutPath);
-// 					// vscode.window.showInformationMessage('Copied to clipboard.');
-// 					vscode.window.showInformationMessage(['Copied to clipboard', finalOutPath].join(': '));
-
-// 				} else {
-// 					output_channel.appendLine("could not get currentFileDottedPath.");
-// 				}
-// 			} catch (e) {
-// 				console.error(e);
-// 				vscode.window.showErrorMessage('Failed to parse file.');
-// 			}
-
-// 		}
-// 		else {
-// 			debug_string.push(`\nNo symbol at line ${lineNumber}`);
-// 			console.log(`No symbol at line ${lineNumber}`);
-// 		}
-// 	} else {
-// 		debug_string.push("No symbols found");
-// 	}
-// 	return debug_string.join("");
-// };
 
 
 async function getBestSurroundingLanguageServerSymbol() {
@@ -331,11 +218,26 @@ async function copyCurrentFilePathWithCurrentLineNumber(markdown: boolean = fals
 	if (includeHighlightedTextAsCodeBlock) {
 		const selectedText = editor.document.getText(editor.selection);
 		if (includeContainingSymbolPath) {
+			// [/c:/Users/pho/repos/Spike3DWorkEnv/pyPhoPlaceCellAnalysis/src/pyphoplacecellanalysis/GUI/PyQtPlot/Widgets/ContainerBased/RankOrderRastersDebugger.py:154](vscode://file/c:/Users/pho/repos/Spike3DWorkEnv/pyPhoPlaceCellAnalysis/src/pyphoplacecellanalysis/GUI/PyQtPlot/Widgets/ContainerBased/RankOrderRastersDebugger.py:154)
+			// ```python
+			// # From `GUI.PyQtPlot.Widgets.ContainerBased.RankOrderRastersDebugger.RankOrderRastersDebugger.find_nearest_time_index.target_time`
+			// find_nearest_time_index
+			// ```
 			let full_symbol_path = await getBestSurroundingLanguageServerSymbol();
-			const containingSymbolPath = "# From `" + full_symbol_path + "`"; // TODO: this is where I get the current symbol path
+			if (!full_symbol_path) {
+				return output;
+			}
+			let updated_full_symbol_path = findSymbolInString(full_symbol_path, selectedText); // Tries to find the selected symbol string in the search string. If found, it truncates the elements after the search string
+			if (updated_full_symbol_path !== null) {
+				// updated_full_symbol_path contains the symbol path truncating the elements following the selection
+				console.log(updated_full_symbol_path); // Outputs: GUI.PyQtPlot.Widgets.ContainerBased.RankOrderRastersDebugger.RankOrderRastersDebugger.find_nearest_time_index
+			} else {
+				console.log("Symbol not found in the string.");
+				updated_full_symbol_path = full_symbol_path;
+			}
+			const containingSymbolPath = "# From `" + updated_full_symbol_path + "`"; // TODO: this is where I get the current symbol path
 			const codeBlock = "```" + document.languageId + "\n" + containingSymbolPath + "\n" + selectedText + "\n```";
 			output += "\n" + codeBlock;
-
 		}
 		else {
 			const codeBlock = "```" + document.languageId + "\n" + selectedText + "\n```";
@@ -359,72 +261,137 @@ async function printAllSymbols(symbols: vscode.DocumentSymbol[]) {
 	}
 }
 
-// async function getContainingSymbol(lineNumber: number, symbols: vscode.DocumentSymbol[]): Promise<vscode.DocumentSymbol | undefined> {
-// 	// Doesn't quite work, seems to get the outer-most symbol and not the innermost one that contains the lines.
-//     for (let symbol of symbols) {
-//         if (symbol.range.start.line <= lineNumber && symbol.range.end.line >= lineNumber) {
-//             return symbol;
-//         } else if (symbol.children) {
-//             let foundSymbol = await getContainingSymbol(lineNumber, symbol.children);
-//             if (foundSymbol)
-//                 return foundSymbol;
-//         }
-//     }
-//     return undefined;
-// }
+
+async function debugPrintCurrentSelectedBestSymbol() {
+	// const markdown: boolean = false;
+	// const includeHighlightedTextAsCodeBlock: boolean = false;
+	// const includeContainingSymbolPath: boolean = false;
+	const useShortenedSubpathIfPossible: boolean = true;
+
+	if (!vscode.workspace.rootPath) {
+		throw new NoWorkspaceOpen;
+	}
+
+	let editor = vscode.window.activeTextEditor;
+	if (!editor) {
+		throw new NoTextEditorOpen;
+	}
+
+	let document = editor.document;
+	if (document.isUntitled) {
+		throw new DocumentIsUntitled;
+	}
+
+	// const path = document.uri.path;
+	// const relativePath = path.replace(vscode.workspace.rootPath, '');
+	// const lineNumber = editor.selection.active.line + 1;
+	// const columnNumber = editor.selection.active.character + 1;
+	// const includeColumn = vscode.workspace.getConfiguration('hipdotUrlSchemeGrabber').get('includeColumn');
+
+	// const url = `vscode://file${path}:${lineNumber}${includeColumn ? `:${columnNumber}` : ''}`;
+	// return markdown ? `[${relativePath}:${lineNumber}${includeColumn ? `:${columnNumber}` : ''}](${url})` : url;
+	// let output = markdown ? `[${relativePath}:${lineNumber}${includeColumn ? `:${columnNumber}` : ''}](${url})` : url;
+	let output = "";
+
+	// if (includeHighlightedTextAsCodeBlock) {
+	const selectedText = editor.document.getText(editor.selection);
+	// if (includeContainingSymbolPath) {
+	// [/c:/Users/pho/repos/Spike3DWorkEnv/pyPhoPlaceCellAnalysis/src/pyphoplacecellanalysis/GUI/PyQtPlot/Widgets/ContainerBased/RankOrderRastersDebugger.py:154](vscode://file/c:/Users/pho/repos/Spike3DWorkEnv/pyPhoPlaceCellAnalysis/src/pyphoplacecellanalysis/GUI/PyQtPlot/Widgets/ContainerBased/RankOrderRastersDebugger.py:154)
+	// ```python
+	// # From `GUI.PyQtPlot.Widgets.ContainerBased.RankOrderRastersDebugger.RankOrderRastersDebugger.find_nearest_time_index.target_time`
+	// find_nearest_time_index
+	// ```
+	let full_symbol_path = await getBestSurroundingLanguageServerSymbol();
+	if (!full_symbol_path) {
+		return output;
+	}
+	let updated_full_symbol_path = findSymbolInString(full_symbol_path, selectedText); // Tries to find the selected symbol string in the search string. If found, it truncates the elements after the search string
+	if (updated_full_symbol_path !== null) {
+		// updated_full_symbol_path contains the symbol path truncating the elements following the selection
+		console.log(updated_full_symbol_path); // Outputs: GUI.PyQtPlot.Widgets.ContainerBased.RankOrderRastersDebugger.RankOrderRastersDebugger.find_nearest_time_index
+	} else {
+		console.log("Symbol not found in the string.");
+		updated_full_symbol_path = full_symbol_path;
+	}
+
+	// Get the last 2 elements
+	if (useShortenedSubpathIfPossible) {
+		const nearest_symbol_subpath = getLastNPeriodSeparatedElements(updated_full_symbol_path, 2);
+		console.log(nearest_symbol_subpath); // Outputs: find_nearest_time_index.target_time
+		updated_full_symbol_path = nearest_symbol_subpath; // use subpath only
+	}
+
+	// const containingSymbolPath = "# From `" + updated_full_symbol_path + "`"; // TODO: this is where I get the current symbol path
+	// const codeBlock = "```" + document.languageId + "\n" + containingSymbolPath + "\n" + selectedText + "\n```";
+	// output += "\n" + codeBlock;
+
+	output += updated_full_symbol_path;
+	// }
+	// else {
+	// 	const codeBlock = "```" + document.languageId + "\n" + selectedText + "\n```";
+	// 	output += "\n" + codeBlock;
+	// }
+	// TODO: optionally de-indent to the appropriate (minimum) level
+	// }
+
+	// Print the output:
+	console.log(output);
+	output_channel.appendLine(output);
+	return output;
+};
 
 
 
-// function copyCurrentLanguageServerSymbols(): string {
-// 	if (!vscode.workspace.rootPath) {
-// 		throw new NoWorkspaceOpen;
-// 	}
-
-// 	let editor = vscode.window.activeTextEditor;
-// 	if (!editor) {
-// 		throw new NoTextEditorOpen;
-// 	}
-
-// 	let document = editor.document;
-// 	if (document.isUntitled) {
-// 		throw new DocumentIsUntitled;
-// 	}
-
-// 	const path = document.uri.path;
-// 	const relativePath = path.replace(vscode.workspace.rootPath, '');
-// 	const lineNumber = editor.selection.active.line + 1;
-// 	const columnNumber = editor.selection.active.character + 1;
-// 	const includeColumn = vscode.workspace.getConfiguration('hipdotUrlSchemeGrabber').get('includeColumn');
-// 	const url = `vscode://file${path}:${lineNumber}${includeColumn ? `:${columnNumber}` : ''}`;
-
-
-// 	// assuming you have a vscode.TextDocument `doc`
-// 	let symbols = await vscode.commands.executeCommand<vscode.DocumentSymbol[]>(
-// 	'vscode.executeDocumentSymbolProvider',
-// 	document.uri
-// 	);
-
-// 	// TODO: print all of the symbols to the debug console
-
-// 	// TODO: find which symbols correspond to the current lineNumber
-
-// 	// // return markdown ? `[${relativePath}:${lineNumber}${includeColumn ? `:${columnNumber}` : ''}](${url})` : url;
-//     // let output = markdown ? `[${relativePath}:${lineNumber}${includeColumn ? `:${columnNumber}` : ''}](${url})` : url;
-
-//     // if (includeHighlightedTextAsCodeBlock) {
-//     //     const selectedText = editor.document.getText(editor.selection);
-//     //     const codeBlock = "```" + document.languageId + "\n" + selectedText + "\n```";
-// 	// 	// TODO: optionally de-indent to the appropriate (minimum) level
-//     //     output += "\n" + codeBlock;
-//     // }
-
-//     // return output;
-// };
 
 
 
 // the symbols array now contains the symbol tree for the document
 // you have to traverse this tree to find the corresponding symbol
+
+// Called every 1 second to update the small status bar item provided by this extension
+async function getCurrentBestSymbolDynamicString(): Promise<string> {
+    // Replace this with your logic to get a continuously refreshing string
+	"symbol-file"
+
+	["symbol-namespace", "symbol-class"]
+	"symbol-field"
+	"symbol-method"
+	"symbol-event"
+	"symbol-variable"
+	"symbol-array"
+
+	"symbol-keyword"
+	
+	"symbol-numeric"
+	"symbol-operator"
+	"symbol-parameter"
+	"symbol-property"
+	"symbol-ruler"
+	"symbol-snippet"
+	"symbol-string"
+	"symbol-misc"
+	symbol_icon_name = "$(file-code)";
+
+	let currSelectedBestSymbol;
+	try {
+		currSelectedBestSymbol = await debugPrintCurrentSelectedBestSymbol();
+	} catch (e) {
+		if (e instanceof NoWorkspaceOpen) {
+		} else if (e instanceof NoTextEditorOpen) {
+		} else if (e instanceof DocumentIsUntitled) {
+		} else {
+			return "<ERR>";
+		}
+	}
+
+	if (!currSelectedBestSymbol) {
+		console.log("ERROR: Could not get file path with line number.");
+		output_channel.appendLine("ERROR: Could not get file path with line number.");
+		return "<ERR>";
+	}
+	return currSelectedBestSymbol;
+}
+
 
 
 
@@ -635,6 +602,53 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(disposable);
 
 	output_channel.show()
+
+
+	// Create a new status bar item that we can now manage
+    let myStatusBarItem: vscode.StatusBarItem;
+    myStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+    myStatusBarItem.command = "extension.debugPrintCurrentSymbols"; // It can be linked to a command if needed
+    myStatusBarItem.show();
+	context.subscriptions.push(myStatusBarItem);
+
+    // Call this function to update the text displayed on the status bar item
+    async function updateStatusBarItem(): Promise<void> {
+        try {
+            const status_bar_text = await getCurrentBestSymbolDynamicString();
+            myStatusBarItem.text = status_bar_text;
+        } catch (error) {
+            console.error('Failed to update status bar item:', error);
+            myStatusBarItem.text = "<err>"; // Or some other placeholder text
+        }
+    }
+
+	// Set up an initial call and a repeating call to keep the status bar updated
+    updateStatusBarItem(); // Initial call to set the status bar item
+    const statusBarUpdateInterval = 1000; // Update the status bar every second
+    const intervalId = setInterval(() => updateStatusBarItem(), statusBarUpdateInterval);
+
+    // Dispose of the interval when the extension is deactivated
+    context.subscriptions.push({
+        dispose: () => {
+            clearInterval(intervalId);
+        }
+    });
+    // function updateStatusBarItem(): void {
+    //     const status_bar_text = getCurrentBestSymbolDynamicString(); // Replace with your function
+    //     myStatusBarItem.text = status_bar_text;
+    // }
+
+    // Register the command it links to (optional)
+    // let disposableStatusBar = vscode.commands.registerCommand('extension.debugPrintCurrentSymbols', () => {
+    //     // This is executed when the status bar item is clicked
+	// 	let curr_best_symbols_string = `Current Best Symbol: ${getCurrentBestSymbolDynamicString()}`
+    //     vscode.window.showInformationMessage(curr_best_symbols_string);
+    // });
+
+    // context.subscriptions.push(disposableStatusBar);
+
+    // Set interval for how often to refresh the status bar item, e.g., every second
+    // setInterval(updateStatusBarItem, 1000);
 
 }
 
